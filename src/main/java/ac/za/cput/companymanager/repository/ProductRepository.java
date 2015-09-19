f96f3fd744b08398ea412f30a6f8fd4cd5cf436a
Yusra94 <yusraismail17@gmail.com>
2015-09-19 23:36:02 +0100

    Deploy

--- src/main/java/ac/za/cput/companymanager/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package ac.za.cput.companymanager.repository;
-
-import ac.za.cput.companymanager.domain.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long>{
-
-}
